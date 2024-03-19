e1dc80cc46af3b2e2af5f42c0803c4ce06c28c57
MicheleIanni86 <micheleianni86@gmail.com>
2024-03-19 19:02:38 +0100

    feat: Add Movies Database

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     // mostra homepage sito
 public function index() 
 {
+    $movies = Movie::all();
+    dd($movies);
     return view('home');
 }
 }
